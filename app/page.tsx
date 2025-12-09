04b5a3b3db46b5527917fb200ef95d567e1888cc
gabrimoreira <gabrieldevandrade49@gmail.com>
2025-12-09 16:28:04 -0300

    feat: add theme and fonts

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="bg-background flex min-h-screen items-center justify-center  font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 h dark:bg-black sm:items-start bg-background">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,7 +12,7 @@ export default function Home() {
           height={20}
           priority
         />
-        <h1>Oi portfolio</h1>
+        <h1 className="bg-primary">Oi portfolio</h1>
       </main>
     </div>
   );
