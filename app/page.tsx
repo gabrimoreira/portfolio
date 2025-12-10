7236ca8a1251d40ed5666c4cd537f3c9c339fd52
gabrimoreira <gabrieldevandrade49@gmail.com>
2025-12-10 09:44:10 -0300

    feat: add gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
 export default function Home() {
   return (
     <div className="bg-background flex min-h-screen items-center justify-center  font-sans dark:bg-black">
       <Navbar />
+      <Hero/>
     </div>
   );
 }
---
+++ app/sections/Hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero(){
+    return(
+        <main className="flex min-h-screen items-center justify-center ">
+            <h1 className="text-6xl md:text-8xl font-bold text-white">
+                Oi, meu nome é <br />
+                <span className="text-gradient">Gabriel</span>
+            </h1>
+        </main>
+    )
+};
