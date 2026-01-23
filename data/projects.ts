3cd8678d849b7019018e58b0fb7aa9578ddd0cf9
gabrimoreira <gabrieldevandrade49@gmail.com>
2026-01-23 13:30:12 -0300

    feat: add census to project.ts

--- data/projects.ts
+++ data/projects.ts
@@ -1,5 +1,15 @@
 
 export const projects = [
+    {
+    title: "AI-Powered OCR Platform",
+    description: "A full-stack application for automated document text extraction. Built with Next.js frontend, NestJS backend, and AI integration.",
+    tags: ["Next.js", "NestJS", "TypeScript", "AI Integration"],
+    
+    repoLink: "https://github.com/gabrimoreira/document-ocr-ai",
+    demoLink: "https://document-ocr-ai.vercel.app/",
+    
+    image: "/projects/ocr.png",
+  },
   {
     title: "Lung Segmentation Web App",
     description: "A specialized web interface for medical image analysis. Built with React, TypeScript, and ShadCN UI, it allows users to upload DICOM files, apply segmentation algorithms (Otsu, Watershed), manually edit contours, and export results.",
@@ -21,14 +31,15 @@ export const projects = [
     
     image: "/projects/d3.png",
   },
-  {
-    title: "AI-Powered OCR Platform",
-    description: "A full-stack application for automated document text extraction. Built with Next.js frontend, NestJS backend, and AI integration.",
-    tags: ["Next.js", "NestJS", "TypeScript", "AI Integration"],
+{
+    title: "Brazil in Data: 2022 Census Scrollytelling",
+    description: "An immersive data storytelling project analyzing Brazil's demographic transition based on the 2022 Census. Built with Next.js and custom D3.js visualizations to render interactive choropleth maps and charts, revealing insights on population aging, racial inequality, and the visibility of Indigenous and Quilombola territories.",
+    tags: ["Next.js", "D3.js", "TypeScript", "Tailwind CSS", "PostgreSQL"],
     
-    repoLink: "https://github.com/gabrimoreira/document-ocr-ai",
-    demoLink: "https://document-ocr-ai.vercel.app/",
+    repoLink: "https://github.com/kelvinleandro/censo-dataviz-react",
+    demoLink: "https://censo-2022-dataviz.vercel.app/",
     
-    image: "/projects/ocr.png",
+    image: "/projects/censo.png",
   },
+
 ];
